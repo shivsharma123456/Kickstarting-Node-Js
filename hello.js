c6011b41b878eadf916f98f3f1b773ecf4c0df7d
Shiv Sharma <shiv.yogesh92@gmail.com>
2023-04-21 18:24:00 +0530

    Create a server run it on port 4000

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+// console.log("hello world");
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('My name is shiv sharma');
+});
+
+server.listen(4000);
