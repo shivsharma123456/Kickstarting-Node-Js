6e86540e66abef75dc6ec874684abc8556aa830d
Shiv Sharma <shiv.yogesh92@gmail.com>
2023-04-22 13:12:35 +0530

    Writing and reading from a file

--- hello.js
+++ hello.js
@@ -1,9 +1,47 @@
-// console.log("hello world");
-
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.end('My name is shiv sharma');
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body>');
+    res.write('<h1>My Messages</h1>');
+    const messages = fs.readFileSync('messages.txt', 'utf8').split('\n');
+    messages.forEach(message => {
+      res.write(`<p>${message}</p>`);
+    });
+    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.appendFileSync('messages.txt', message + '\n');
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
